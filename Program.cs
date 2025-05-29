e5ad8605a308c8ff13d8bdce4452e018224e5892
Pedro Henrique <ph6704938@gmail.com>
2025-05-28 22:32:37 -0300

    feat: add Swagger config, DbContext, and repository services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
+using FloodWatch.Domain.Entities;
+using FloodWatch.Infrastructure.Context;
+using FloodWatch.Infrastructure.Persistence.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +12,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = builder.Configuration["Swagger:Title"],
+        Description = builder.Configuration["Swagger:Description"] + DateTime.Now.Year,
+        Contact = new OpenApiContact()
+        {
+            Email = builder.Configuration["Swagger:Email"],
+            Name = builder.Configuration["Swagger:Name"]
+        }
+    });
+});
+
+builder.Services.AddDbContext<FloodWatchContext>(options =>
+{
+    options.UseOracle(builder.Configuration.GetConnectionString("Oracle"));
+});
+
+builder.Services.AddScoped<IRepository<Alert>, Repository<Alert>>();
+builder.Services.AddScoped<IRepository<Sensor>, Repository<Sensor>>();
+builder.Services.AddScoped<IRepository<SensorReading>, Repository<SensorReading>>();
 
 var app = builder.Build();
 
