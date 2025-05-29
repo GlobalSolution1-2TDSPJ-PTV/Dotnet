8da67cda5822a7ab979bf7244e9395f06f7c7eaa
Pedro Henrique <ph6704938@gmail.com>
2025-05-28 22:34:05 -0300

    feat: add migration V1 with Sensors, Alerts, and Readings tables

---
+++ Migrations/20250529012509_V1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FloodWatch.Migrations
+{
+    /// <inheritdoc />
+    public partial class V1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sensors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    Type = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Localizacao = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Latitude = table.Column<double>(type: "BINARY_DOUBLE", nullable: false),
+                    Longitude = table.Column<double>(type: "BINARY_DOUBLE", nullable: false),
+                    IsActive = table.Column<bool>(type: "NUMBER(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sensors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Alerts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    SensorId = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    Type = table.Column<string>(type: "NVARCHAR2(50)", maxLength: 50, nullable: false),
+                    Message = table.Column<string>(type: "NVARCHAR2(255)", maxLength: 255, nullable: false),
+                    Level = table.Column<string>(type: "NVARCHAR2(20)", maxLength: 20, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    IsResolved = table.Column<bool>(type: "NUMBER(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alerts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Alerts_Sensors_SensorId",
+                        column: x => x.SensorId,
+                        principalTable: "Sensors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SensorReadings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    SensorId = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    SensorValue = table.Column<double>(type: "BINARY_DOUBLE", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorReadings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SensorReadings_Sensors_SensorId",
+                        column: x => x.SensorId,
+                        principalTable: "Sensors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alerts_SensorId",
+                table: "Alerts",
+                column: "SensorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorReadings_SensorId",
+                table: "SensorReadings",
+                column: "SensorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alerts");
+
+            migrationBuilder.DropTable(
+                name: "SensorReadings");
+
+            migrationBuilder.DropTable(
+                name: "Sensors");
+        }
+    }
+}
