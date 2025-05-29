653e8133e881790b3a7924244ee242b4c3e6c69c
Pedro Henrique <ph6704938@gmail.com>
2025-05-28 22:31:09 -0300

    feat: add FloodWatchContext for Entity Framework integration

---
+++ Infrastructure/Context/FloodWatchContext.cs
@@ -0,0 +1,20 @@
+﻿using FloodWatch.Domain.Entities;
+using FloodWatch.Infrastructure.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace FloodWatch.Infrastructure.Context
+{
+    public class FloodWatchContext(DbContextOptions<FloodWatchContext> options) : DbContext(options)
+    {
+
+        public DbSet<Sensor> Sensors { get; set; }
+        public DbSet<SensorReading> SensorReadings { get; set; }
+        public DbSet<Alert> Alerts { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new SensorMapping());
+            modelBuilder.ApplyConfiguration(new SensorReadingMapping());
+            modelBuilder.ApplyConfiguration(new AlertMapping());
+        }
+    }
+}
